eb702af86a0c042be674d116bc8574dd0be9f1e5
tankw <tantk113@gmail.com>
2023-09-25 21:12:43 +0700

    async readfile

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const { readFile } = require('fs')
+const { readFile, writeFile } = require('fs')
 
-readFile('./sub-fol/text.txt', (e,r) =>{if (e) {
-  console.log(e)
-  return 
-}})
+readFile('./sub-fol/text.txt', 'utf-8', (e, r) => {
+  if (e) {
+    console.log(e)
+    return
+  }
+  const first = r;
+  readFile('./sub-fol/secondsubfol/2nd.txt', 'utf8', (e, r) => {
+    if (e) {
+      console.log(e)
+      return
+    }
+    const second = r;
+    writeFile('./sub-fol/result-async.txt', `here is the rewrweesult${first}, ${second}`, (e, r) => {
+      if (e) {
+        console.log(e);
+        return;
+      }
+      console.log(r);
+    })
+
+  })
+
+})
