c1df9f062dd300a11a805aa41b405836f1346a3d
tankw <tantk113@gmail.com>
2023-09-25 23:52:42 +0700

    http

--- app.js
+++ app.js
@@ -1,25 +1,10 @@
-const { readFile, writeFile } = require('fs')
-
-readFile('./sub-fol/text.txt', 'utf-8', (e, r) => {
-  if (e) {
-    console.log(e)
-    return
-  }
-  const first = r;
-  readFile('./sub-fol/secondsubfol/2nd.txt', 'utf8', (e, r) => {
-    if (e) {
-      console.log(e)
-      return
-    }
-    const second = r;
-    writeFile('./sub-fol/result-async.txt', `here is the rewrweesult${first}, ${second}`, (e, r) => {
-      if (e) {
-        console.log(e);
-        return;
-      }
-      console.log(r);
-    })
-
-  })
-
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.write('hellow')
+  res.end()
 })
+
+
+
+
+server.listen(5000);
