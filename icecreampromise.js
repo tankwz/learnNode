8357415239b1eeb0b4efa11d266db71c87502f53
tankw <tantk113@gmail.com>
2023-09-28 12:38:03 +0700

    tree

--- icecreampromise.js
+++ icecreampromise.js
@@ -1,7 +1,31 @@
 
 let stocks = {
-  Fruits : ["strawberry", "grapes", "banana", "apple"],
-  liquid : ["water", "ice"],
-  holder : ["cone", "cup", "stick"],
-  toppings : ["chocolate", "peanuts"],
+  Fruits: ["strawberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
 };
+
+const is_shop_open = true;
+
+const order = (time, work) => {
+  return new Promise((resolve, reject)=>{
+    if(is_shop_open){
+      setTimeout(() => {
+        resolve(work())
+      }, time);
+    }
+    else {
+      reject(console.log("closed"))
+    }
+  })
+}
+
+
+order(2000, ()=> console.log(`${stocks.Fruits[0]}`))
+  .then(()=>{
+    return order(0,()=> console.log("product starting"));
+  })
+  .then(()=>{
+    return order(2000, ()=>console.log('fruit chopped'));
+  })
