b3f7d1b89c321dfcd00d9c0895087bc6ba84919b
tankw <tantk113@gmail.com>
2023-09-29 19:33:50 +0700

    httpsetup

--- node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/02-express-tutorial/app.js
@@ -1,1 +1,41 @@
-console.log('Express Tutorial')
+const http = require('http');
+const { readFileSync } = require('fs');
+
+//get all file
+const homepage = readFileSync('./navbar-app/index.html');
+const homepagestyle = readFileSync('./navbar-app/styles.css');
+const homepagejs = readFileSync('./navbar-app/browser-app.js');
+const homepagelogo = readFileSync('./navbar-app/logo.svg');
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(homepage);
+    res.end();
+  } else if (url === '/styles.css') {
+    res.writeHead(200, { 'content-type': 'text/css' });
+    res.write(homepagestyle);
+    res.end();
+  } else if (url === '/browser-app.js') {
+    res.writeHead(200, { 'content-type': 'text/javascript' });
+    res.write(homepagejs);
+    res.end();
+  } else if (url === '/logo.svg') {
+    res.writeHead(200, { 'content-type': 'image/svg+xml' });
+    res.write(homepagelogo);
+    res.end();
+  } else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' });
+    res.write(`<h1>about page </h1>`);
+    res.end();
+  } else {
+    res.writeHead(404, { 'content-type': 'text/html' });
+    res.write(`<h1>error 404: page not found</h1>`);
+    res.end();
+  }
+});
+
+server.listen(5000, () => {
+  console.log('listening at 5000');
+});
