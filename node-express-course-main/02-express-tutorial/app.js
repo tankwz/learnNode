46861917625b27be6579eda1ea2abbaf33ecf0e6
tankw <tantk113@gmail.com>
2023-09-30 23:08:07 +0700

    middleware

---
+++ node-express-course-main/02-express-tutorial/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const logger = require('./logger');
+const authorize = require('./authorize');
+
+//app.use('/', logger, authorize);
+app.use('/api', [logger, authorize]);
+
+app.get('/', (req, res) => {
+  res.send('Home');
+});
+
+app.get('/about', (req, res) => {
+  res.send('about');
+});
+
+app.get('/api/about', (req, res) => {
+  console.log(req.user);
+  res.send('about');
+});
+
+app.listen(5000, () => {
+  console.log('listening');
+});
